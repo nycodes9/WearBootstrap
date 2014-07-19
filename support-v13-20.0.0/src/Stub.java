f35188bac7c8537dccd749719673bc9fc58810b1
raj <nycodes9@gmail.com>
2014-07-19 14:19:43 -0700

    doc and src for wear UI lib, v4, v13 and google play services

---
+++ support-v13-20.0.0/src/Stub.java
@@ -0,0 +1,6 @@
+import android.support.v13.app.FragmentPagerAdapter;
+
+
+public class Stub {
+//	FragmentPagerAdapter
+}
---
+++ support-v4-20.0.0/src/Stub.java
@@ -0,0 +1,7 @@
+import android.support.v4.view.ViewPager;
+
+
+public class Stub {
+//	ViewPager
+
+}
